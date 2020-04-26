ea38d219e4f51019a8c8d43cd22546efdd97fbcf
kumagai <iariro@gol.com>
2020-04-26 12:59:35 +0900

    GitHub Actions対応

--- corona/corona.py
+++ corona/corona.py
@@ -1,9 +1,8 @@
-# coding: utf-8
 #!/usr/local/bin/python3
 import datetime
 import os
 import sys
-from bottle import route, run, template, static_file, get, request
+from bottle import route, template, request
 dirpath = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(dirpath)
 os.chdir(dirpath)
@@ -11,38 +10,38 @@ import coronadata
 
 @route('/')
 def index():
-	return template('index.html')
+    return template('index.html')
 
 @route('/input')
 def input():
-	return template('input.html', date=datetime.datetime.now().strftime('%Y/%m/%d'))
+    return template('input.html', date=datetime.datetime.now().strftime('%Y/%m/%d'))
 
 @route('/input2', method="POST")
 def input2():
-	try:
-		date = request.POST.getunicode('date')
-		infect_num = request.POST.getunicode('infect_num')
-		coronadata.add(date, infect_num)
-		return template('input2.html', date=date, infect_num=infect_num)
-	except Exception as e:
-		return str(e)
+    try:
+        date = request.POST.getunicode('date')
+        infect_num = request.POST.getunicode('infect_num')
+        coronadata.add(date, infect_num)
+        return template('input2.html', date=date, infect_num=infect_num)
+    except Exception as e:
+        return str(e)
 
 @route('/graph')
 def graph():
-	try:
-		end_date = coronadata.last_complete_week_start(datetime.datetime.today())
-		daily_num = coronadata.read_data()
-		weeks, weekly_num = coronadata.statistic_weekly(daily_num, datetime.datetime(2020, 3, 2), end_date)
-		weekly_sum = coronadata.sum_weekly(daily_num)
-		return template('graph.html',
-			daily_num_x=[day.strftime('%Y/%m/%d') for day in daily_num],
-			daily_num_y=list(daily_num.values()),
+    try:
+        end_date = coronadata.last_complete_week_start(datetime.datetime.today())
+        daily_num = coronadata.read_data()
+        weeks, weekly_num = coronadata.statistic_weekly(daily_num, datetime.datetime(2020, 3, 2), end_date)
+        weekly_sum = coronadata.sum_weekly(daily_num)
+        return template('graph.html',
+            daily_num_x=[day.strftime('%Y/%m/%d') for day in daily_num],
+            daily_num_y=list(daily_num.values()),
 
-			weekly_sum_x=[day.strftime('%Y/%m/%d') for day in weekly_sum.keys()],
-			weekly_sum_y=list(weekly_sum.values()),
+            weekly_sum_x=[day.strftime('%Y/%m/%d') for day in weekly_sum.keys()],
+            weekly_sum_y=list(weekly_sum.values()),
 
-			weekly_num_x=weeks,
-			weekly_num_y=[{'name': day.strftime('%Y/%m/%d'), 'data': values} for day, values in weekly_num.items()],
-			weekly_num=weekly_num)
-	except Exception as e:
-		return str(e)
+            weekly_num_x=weeks,
+            weekly_num_y=[{'name': day.strftime('%Y/%m/%d'), 'data': values} for day, values in weekly_num.items()],
+            weekly_num=weekly_num)
+    except Exception as e:
+        return str(e)
