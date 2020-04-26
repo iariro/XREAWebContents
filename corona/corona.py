4dcf1457c3d4e8505b13b4abe9c6dab5ff879eb9
iariro <iariro@gol.com>
2020-04-27 01:45:00 +0900

    Update corona.py

--- corona/corona.py
+++ corona/corona.py
@@ -34,14 +34,12 @@ def graph():
         weeks, weekly_num = coronadata.statistic_weekly(daily_num, datetime.datetime(2020, 3, 2), end_date)
         weekly_sum = coronadata.sum_weekly(daily_num)
         return template('graph.html',
-            daily_num_x=[day.strftime('%Y/%m/%d') for day in daily_num],
-            daily_num_y=list(daily_num.values()),
-
-            weekly_sum_x=[day.strftime('%Y/%m/%d') for day in weekly_sum.keys()],
-            weekly_sum_y=list(weekly_sum.values()),
-
-            weekly_num_x=weeks,
-            weekly_num_y=[{'name': day.strftime('%Y/%m/%d'), 'data': values} for day, values in weekly_num.items()],
-            weekly_num=weekly_num)
+                        daily_num_x=[day.strftime('%Y/%m/%d') for day in daily_num],
+                        daily_num_y=list(daily_num.values()),
+                        weekly_sum_x=[day.strftime('%Y/%m/%d') for day in weekly_sum.keys()],
+                        weekly_sum_y=list(weekly_sum.values()),
+                        weekly_num_x=weeks,
+                        weekly_num_y=[{'name': day.strftime('%Y/%m/%d'), 'data': values} for day, values in weekly_num.items()],
+                        weekly_num=weekly_num)
     except Exception as e:
         return str(e)
