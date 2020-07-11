0cd1d2ab7928999a223ceebf650878dfdbd57768
kumagai <iariro@gol.com>
2020-07-11 13:23:52 +0900

    Bottle導入

--- ondotori/ondotori.py
+++ ondotori/ondotori.py
@@ -58,8 +58,9 @@ def graph_all():
                                                                          remote_serial)
         weekly = ondotoridata.getProcessedDaySeries(weekly, mean)
         monthly = ondotoridata.getProcessedDaySeries(monthly, mean)
-        annual = ondotoridata.getMeanOfDaySeriesPerYear(daily, start_date='2019/07/01', mean_range=9)
-
+        annual = ondotoridata.getMeanOfDaySeriesPerYear(daily,
+                                                        start_date='2019/07/01',
+                                                        mean_range=9)
         return template('graph_all.html', weekly=weekly, monthly=monthly, annual=annual)
     except Exception as e:
         return str(e)
