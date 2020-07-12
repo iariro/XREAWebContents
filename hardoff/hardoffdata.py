5111ea2702820fcf6f77e1c54b1794e4f7abd52a
kumagai <iariro@gol.com>
2020-07-12 12:10:10 +0900

    hardoff:Python化

--- hardoff/hardoffdata.py
+++ hardoff/hardoffdata.py
@@ -10,7 +10,7 @@ def query(sql):
     :param sql: SQL string
     '''
     conn = MySQLdb.connect(user='iariro', passwd='abc123', host='localhost', db='iariro', charset='utf8')
-    cur = conn.cursor()
+    cur = conn.cursor(MySQLdb.cursors.DictCursor)
     cur.execute(sql)
     rows = cur.fetchall()
     cur.close()
@@ -19,72 +19,70 @@ def query(sql):
     return rows
 
 def get_area_count():
-    rows = query("SELECT SUBSTRING(address,1, CASE WHEN locate('県',address)<>0 THEN locate('県',address) WHEN locate('府',address)<>0 THEN locate('府',address) WHEN locate('都',address)<>0 THEN locate('都',address) END) as prefecture, count(visit_date), count(*) FROM iariro.ho_store group by prefecture;")
+    rows = query("SELECT SUBSTRING(address,1, CASE WHEN locate('県',address)<>0 THEN locate('県',address) WHEN locate('府',address)<>0 THEN locate('府',address) WHEN locate('都',address)<>0 THEN locate('都',address) END) as prefecture, count(visit_date) as count_visit_date, count(*) as count_all FROM iariro.ho_store group by prefecture;")
 
     prefectures = []
     visited = []
     unvisited = []
     for row in rows:
-        prefectures.append(row[0])
-        visited.append(row[1])
-        unvisited.append(row[2])
+        prefectures.append(row['prefecture'])
+        visited.append(row['count_visit_date'])
+        unvisited.append(row['count_all'])
     return (prefectures, visited, unvisited)
 
 def get_completion_history():
     rows = query("SELECT * FROM ho_store where visit_date is not null;")
+
     dates = []
     for row in rows:
         dates.append(row['visit_date'])
 
-    monthlycount = array();
-    monthlycount2 = array();
+    monthlycount = {}
+    monthlycount2 = {}
     nowyear = datetime.datetime.now().year
     d2 = None
-    for year in range(2018, nowyear+2):
+    for year in range(2018, nowyear + 2):
         for month in range(1, 12 + 1):
-            if year == nowyear+1 and month>1:
+            if year == nowyear + 1 and month > 1:
                 break
             d = datetime.datetime(year=year, month=month, day=1)
             count = 0
             count2 = 0
             for date in dates:
+                date = datetime.datetime.combine(date, datetime.time())
                 if date < d:
                     count2 += 1
                     if date >= d2:
                         count += 1
 
-            if d is not None:
+            if d2 is not None:
                 monthlycount[d2] = count
                 monthlycount2[d2] = count2
             d2 = d
 
-    bardata = [[month, count] for month, count in monthlycount.items()]
-    linedata = [[month, count] for month, count in monthlycount2.items()]
+    bardata = [[month.timestamp() * 1000, count] for month, count in monthlycount.items()]
+    linedata = [[month.timestamp() * 1000, count] for month, count in monthlycount2.items()]
 
     return bardata, linedata
 
 def update_store(store_id, near_station, minutes_from_near_station, visit_date, address, targeting):
     values = []
-    if near_station is None and minutes_from_near_station is not None:
+    if near_station is not None and minutes_from_near_station is not None:
         values.append("near_station='%s'" % near_station)
         values.append("minutes_from_near_station=%d" % minutes_from_near_station)
 
     if visit_date is not None:
         values.append("visit_date='%s'" % visit_date)
-    else:
-        values.append("visit_date=null")
 
     if address is not None:
         values.append("address='%s'" % address)
-    else:
-        values.append("visit_date=null")
 
-    if targeting is not None:
+    if targeting is not None and len(targeting) > 0:
         values.append("targeting='target'")
-    else:
-        values.append("targeting=null")
 
-    query("update ho_store set %s where store_id=%d" % (','.join(values), store_id))
+    sql = "update ho_store set %s where store_id=%s" % (','.join(values), store_id)
+    query(sql)
+    return sql
 
 def get_store_list(where):
     where_clause = {'visited': ' where visit_date is not null',
@@ -94,7 +92,7 @@ def get_store_list(where):
 
     sql = "SELECT * FROM ho_store"
     if where in where_clause:
-        sql += where[where]
+        sql += where_clause[where]
     if where == 'visited_day':
         sql += ' order by visit_date desc'
 
@@ -103,16 +101,17 @@ def get_store_list(where):
     days = []
     day = None
     for row in rows:
-        if where == 'visited_day' and pday != None and pday != row["visit_date"]:
+        if day is None or (where == 'visited_day' and pday != None and pday != row["visit_date"]):
             day = {'date': row["visit_date"], 'values': []}
             days.append(day)
         day['values'].append(row)
+        pday = row["visit_date"]
     return len(rows), days
 
-def update_visit_date():
-    today = dateime.datetime.now().strftime('%Y/%m/%d')
+def update_visit_date(store_id, date):
+    today = date.strftime('%Y/%m/%d')
 
-    sql = "update ho_store set visit_date='%s' where store_id=%d" % (today, store_id)
+    sql = "update ho_store set visit_date='%s' where store_id=%s" % (today, store_id)
     query(sql)
 
 ######################################################################
@@ -121,16 +120,35 @@ def update_visit_date():
 
 class HardoffDBTest(unittest.TestCase):
     def test_get_area_count(self):
-        get_area_count()
+        (prefectures, visited, unvisited) = get_area_count()
+        self.assertIsNotNone(get_area_count())
 
     def test_get_completion_history(self):
-        get_completion_history()
+        self.assertIsNotNone(get_completion_history())
 
-#   def test_update_store(self):
-#       update_store(store_id, near_station, minutes_from_near_station, visit_date, targeting)
+    def test_update_store(self):
+        print(update_store('1', None, None, '2020/07/12', None, None))
+#       update_store(store_id, near_station, minutes_from_near_station, visit_date, address, targeting):
 
-    def test_get_store_list(self):
-        get_store_list(where)
+    def test_get_store_list_visited(self):
+        count, days = get_store_list('visited')
+        for day in days:
+            print(day['date'])
 
-#   def test_update_visit_date(self):
-#       update_visit_date()
+    def test_get_store_list_visited_day(self):
+        count, days = get_store_list('visited_day')
+        for day in days:
+            print(day['date'])
+
+    def test_get_store_list_unvisited(self):
+        count, days = get_store_list('unvisited')
+        for day in days:
+            print(day['date'])
+
+    def test_get_store_list_target(self):
+        count, days = get_store_list('target')
+        for day in days:
+            print(day['date'])
+
+    def test_update_visit_date(self):
+        update_visit_date('1', datetime.date.today())
---
+++ hardoff/hardoffview.py
@@ -0,0 +1,66 @@
+﻿from bottle import route, template, request
+import datetime
+import hardoffdata
+
+@route('/')
+def index():
+    return template('index.html')
+
+@route('/area_count')
+def area_count():
+    (prefectures, visited, unvisited) = hardoffdata.get_area_count()
+    prefectures = ','.join(["'%s'" % v for v in prefectures])
+    visited = ','.join([str(v) for v in visited])
+    unvisited = ','.join([str(v) for v in unvisited])
+    return template('area_count.html', prefectures=prefectures, visited=visited, unvisited=unvisited)
+
+@route('/completion_history')
+def completion_history():
+    (bardata, linedata) = hardoffdata.get_completion_history()
+    bardata = ','.join([str(v) for v in bardata])
+    linedata =','.join( [str(v) for v in linedata])
+    return template('completion_history.html', bardata=bardata, linedata=linedata)
+
+@route('/store_edit1', method='POST')
+def store_edit1():
+    store_id = request.POST.getunicode('store_id')
+    name = request.POST.getunicode('name')
+    near_station = request.POST.getunicode('near_station')
+    minutes_from_near_station = request.POST.getunicode('minutes_from_near_station')
+    visit_date = request.POST.getunicode('visit_date')
+    address = request.POST.getunicode('address')
+    targeting = request.POST.getunicode('targeting')
+    return template('store_edit1.html',
+                    store_id=store_id,
+                    name=name,
+                    near_station=near_station,
+                    minutes_from_near_station=minutes_from_near_station,
+                    visit_date=visit_date,
+                    address=address,
+                    targeting=targeting)
+
+@route('/store_edit2', method='POST')
+def store_edit2():
+    store_id = request.POST.getunicode('store_id')
+    near_station = request.POST.getunicode('near_station')
+    minutes_from_near_station = request.POST.getunicode('minutes_from_near_station')
+    visit_date = request.POST.getunicode('visit_date')
+    address = request.POST.getunicode('address')
+    targeting = request.POST.getunicode('targeting')
+    try:
+        sql = hardoffdata.update_store(store_id, near_station, minutes_from_near_station, visit_date, address, targeting)
+        return template('store_edit2.html', success=True, sql=sql)
+    except:
+        return template('store_edit2.html', success=False, sql=sql)
+
+@route('/store_list', method='POST')
+def store_list():
+    where = request.POST.getunicode('where')
+    (count, days) = hardoffdata.get_store_list(where)
+    return template('store_list.html', count=count, days=days)
+
+@route('/store_visit_today', method='POST')
+def store_visit_today():
+    store_id = request.POST.getunicode('store_id')
+    hardoffdata.update_visit_date(store_id, datetime.date.today())
+    return template('store_visit.html')
