f6aaa233d483f4638de023674405cda60b5e9c5d
kumagai <iariro@gol.com>
2020-10-07 04:18:13 +0900

    直前の値比較＋LINE通知を実装

--- ondotori/ondotori_battery.py
+++ ondotori/ondotori_battery.py
@@ -5,25 +5,25 @@ import ambient
 import requests
 
 def get_battery():
-	login_id = 'tbac0004'
-	login_pass = 'bukkuden'
-	devices = ondotoridata.getCurrentDataFromWebStorage(login_id, login_pass)
-	for device in devices['devices']:
-		if device['serial'] == '5214C18D':
-			return int(device['battery'])
+    login_id = 'tbac0004'
+    login_pass = 'bukkuden'
+    devices = ondotoridata.getCurrentDataFromWebStorage(login_id, login_pass)
+    for device in devices['devices']:
+        if device['serial'] == '5214C18D':
+            return int(device['battery'])
 
 def line_notify(last, now):
-	token = "XXXXXXXXXXXXXXXXXXXXXXXX"
-	url = "https://notify-api.line.me/api/notify"
-	headers = {"Authorization": "Bearer " + token}
-	payload = {"message": 'おんどとりの電池残量が%dから%dになりました' % (last, now)}
-	requests.post(url, headers=headers, data=payload)
+    token = "nPQEoC190nfvydJRbQmY75SY00Ygvt0CxsaXWoLTUUH"
+    url = "https://notify-api.line.me/api/notify"
+    headers = {"Authorization": "Bearer " + token}
+    payload = {"message": 'おんどとりの電池残量が%dから%dになりました' % (last, now)}
+    requests.post(url, headers=headers, data=payload)
 
 if __name__ == '__main__':
-	amb = ambient.Ambient(27153, 'e26f5bd118ca9cb7', '5ec1d2977853fa8c')
-	last = amb.read(n=1)[0]['d1']
-	now = get_battery()
-	amb.send({'d1': now})
+    amb = ambient.Ambient(27153, 'e26f5bd118ca9cb7', '5ec1d2977853fa8c')
+    last = amb.read(n=1)[0]['d1']
+    now = get_battery()
+    amb.send({'d1': now})
 
-	if now != last:
-		line_notify(last, now)
+    if now != last:
+        line_notify(last, now)
