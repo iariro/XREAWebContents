bc807f37f60cbbdf26d46b5c2be509e7a9fb2050
kumagai <iariro@gol.com>
2024-03-15 00:47:02 +0900

    maajan init

---
+++ maajan/bottle.cgi
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+import os
+import sys
+from bottle import run
+dirpath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(dirpath)
+os.chdir(dirpath)
+import tenpai_view # noqa F401
+
+if __name__ == '__main__':
+    DEBUG = os.path.exists(os.path.expanduser('~/debug'))
+    if DEBUG:
+        run(host='localhost', port=8080, debug=True)
+    else:
+        run(host='0.0.0.0', port=80, server="cgi")
---
+++ maajan/tenpai_view.py
@@ -0,0 +1,42 @@
+#!/usr/local/bin/python3
+from bottle import route, template, request, run, view, static_file, url
+
+import sys
+sys.path.append("../../../python/")
+
+from maajanlib.logic.Pai import Pai
+from maajanlib.logic.PaiKind import PaiKind, PaiKindShort
+from maajanlib.logic.MachiPattern import MachiPattern
+
+pai_image_list = []
+pai_image_list += ['man_{}.png'.format(i) for i in range(1, 10)]
+pai_image_list += ['pin_{}.png'.format(i) for i in range(1, 10)]
+pai_image_list += ['sou_{}.png'.format(i) for i in range(1, 10)]
+pai_image_list += ['kaze_ton.png', 'kaze_nan.png', 'kaze_sha.png', 'kaze_pei.png',
+                   'sangen_haku.png', 'sangen_hatu.png', 'sangen_chun.png',]
+
+@route('/image/<filepath:path>', name='static_file')
+def static(filepath):
+    return static_file(filepath, root="./views/image")
+
+@route('/')
+def index():
+    return template('index.html')
+
+@route('/tenpai', method='POST')
+def tenpai():
+    try:
+        tehai_line = request.POST.getunicode('tehai')
+        tehai = [Pai(PaiKindShort.value_of(p)) for p in tehai_line.split()]
+        tehai_img = [pai_image_list[PaiKindShort.value_of(p)] for p in tehai_line.split()]
+
+        machiPattern = MachiPattern(tehai, True)
+        machi_detail = []
+        for element in machiPattern.machiElementCollection.machi_element_list:
+            machi_detail.append({'type': element.type_jp.name, 'pai_list': [pai_image_list[m] for m in element.pai_list]})
+        machi = machiPattern.getMachi()
+        machi_img = [pai_image_list[m] for m in machi]
+
+        return template('tenpai.html', tehai=tehai_img, machi=machi_img, machi_detail=machi_detail)
+    except Exception as e:
+        return str(e)
