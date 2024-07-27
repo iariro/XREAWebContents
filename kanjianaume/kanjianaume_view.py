7ade37ae0d94832f8d9db0917d4f4f72b8fd0cd7
kumagai <iariro@gol.com>
2024-07-27 14:35:16 +0900

    表示調整

--- kanjianaume/kanjianaume_view.py
+++ kanjianaume/kanjianaume_view.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
-from bottle import route, template, request, run, view, static_file, url
+from bottle import route, template, request, run, view, static_file, url, debug
+debug(True)
 
 import sys
 import kanjianaume
@@ -16,32 +17,30 @@ def index():
 
 @route('/makequestion', method='POST')
 def makequestion():
-    try:
-        word_list = kanjianaume.read_word()
-        black_list = kanjianaume.read_black_list()
-        word_list = kanjianaume.delete_invalid_word(word_list, black_list)
-        valid_chars = kanjianaume.read_valid_chars()
-        qword, qc1, qc2, valid = kanjianaume.make_question(word_list, valid_chars)
-        if qword and qc1 and qc2:
-            solved_chars = kanjianaume.solve(word_list, qc1, qc2)
-            return template('question.html',
-                            qword1=' '.join(qword[0]),
-                            qword2=' '.join(qword[1]),
-                            qc1_qc2=' '.join(qc1 + qc2),
-                            chars=[qc2[0][0], qc2[1][0], qc1[0][1], qc1[1][1]],
-                            center_char=qc1[0][0],
-                            solved_chars=' '.join(solved_chars),
-                            valid='valid' if valid else 'invalid')
-        else:
-            return template('question.html',
-                            qword1=' '.join(qword[0]),
-                            qword2=' '.join(qword[1]),
-                            chars=['-', '-', '-', '-'],
-                            center_char=qc1[0][0],
-                            solved_chars='-',
-                            valid='valid' if valid else 'invalid')
-    except Exception as e:
-        return str(e)
+    word_list = kanjianaume.read_word()
+    black_list = kanjianaume.read_black_list()
+    word_list = kanjianaume.delete_invalid_word(word_list, black_list)
+    valid_chars = kanjianaume.read_valid_chars()
+    qword, qc1, qc2, valid = kanjianaume.make_question(word_list, valid_chars)
+    if qword and qc1 and qc2:
+        solved_chars = kanjianaume.solve(word_list, qc1, qc2)
+        return template('question.html',
+                        qword1=' '.join(qword[0]),
+                        qword2=' '.join(qword[1]),
+                        qc1_qc2=' '.join(qc1 + qc2),
+                        chars=[qc2[0][0], qc2[1][0], qc1[0][1], qc1[1][1]],
+                        center_char=qc1[0][0],
+                        solved_chars=' '.join(solved_chars),
+                        valid='valid' if valid else 'invalid')
+    else:
+        return template('question.html',
+                        qword1=' '.join(qword[0]),
+                        qword2=' '.join(qword[1]),
+                        qc1_qc2='-',
+                        chars=['-', '-', '-', '-'],
+                        center_char='-',
+                        solved_chars='-',
+                        valid='valid' if valid else 'invalid')
 
 @route('/blacklist', method='POST')
 def blacklist():
